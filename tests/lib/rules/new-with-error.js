4b2314f9a9cf3fe987c98201f51769ded1ac9cbc
Rich Trott <rtrott@gmail.com>
2020-10-14 06:42:08 -0700

    chore: add standard (#9)

--- tests/lib/rules/new-with-error.js
+++ tests/lib/rules/new-with-error.js
@@ -4,48 +4,47 @@
  * @copyright 2015 Rich Trott. All rights reserved.
  * See LICENSE file in root directory for full license.
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/new-with-error"),
+var rule = require('../../../lib/rules/new-with-error')
 
-    RuleTester = require("eslint").RuleTester;
+var RuleTester = require('eslint').RuleTester
 
-
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Tests
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("new-with-error", rule, {
+var ruleTester = new RuleTester()
+ruleTester.run('new-with-error', rule, {
 
-    valid: [
-        {
-            code: "throw new Error();"
-        },
-        {
-            code: "throw SyntaxError();",
-            options: ['Error']
-        }
-    ],
+  valid: [
+    {
+      code: 'throw new Error();'
+    },
+    {
+      code: 'throw SyntaxError();',
+      options: ['Error']
+    }
+  ],
 
-    invalid: [
-        {
-            code: "throw Error();",
-            errors: [{
-                message: "Use new keyword when throwing.",
-                type: "ThrowStatement"
-            }]
-        },
-        {
-            code: "throw SyntaxError();",
-            errors: [{
-                message: "Use new keyword when throwing.",
-                type: "ThrowStatement"
-            }]
-        }
-    ]
-});
+  invalid: [
+    {
+      code: 'throw Error();',
+      errors: [{
+        message: 'Use new keyword when throwing.',
+        type: 'ThrowStatement'
+      }]
+    },
+    {
+      code: 'throw SyntaxError();',
+      errors: [{
+        message: 'Use new keyword when throwing.',
+        type: 'ThrowStatement'
+      }]
+    }
+  ]
+})
